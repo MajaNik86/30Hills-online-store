4455023f85cf11a0f8caa1fe95ba01e8eca59021
MajaNik86 <majanik86@gmail.com>
2023-11-16 11:40:30 +0100

    added the button to sort products by price

--- models/Product.model.js
+++ models/Product.model.js
@@ -51,6 +51,10 @@ class Product {
   
     return product;
   }
+
+  parsePrice() {
+    return parseFloat(this.price);
+  }
   
 }
 
---
+++ public/scripts/script.js
@@ -0,0 +1,27 @@
+
+const sortButton = document.getElementById('sortButton');
+let ascendingOrder = true;
+
+function sortProductsByPrice() {
+  const productList = document.getElementById('products-grid');
+  const products = Array.from(productList.children);
+
+  products.sort((a, b) => {
+    const priceA = parseFloat(a.querySelector('.product-price').getAttribute('data-price'));
+    const priceB = parseFloat(b.querySelector('.product-price').getAttribute('data-price'));
+
+    // Toggle between ascending and descending order
+    return ascendingOrder ? priceA - priceB : priceB - priceA;
+  });
+
+  // Reverse the sorting order for the next click
+  ascendingOrder = !ascendingOrder;
+
+  // Remove existing list items
+  products.forEach(product => productList.removeChild(product));
+
+  // Append sorted products
+  products.forEach(product => productList.appendChild(product));
+}
+
+sortButton.addEventListener('click', sortProductsByPrice);
